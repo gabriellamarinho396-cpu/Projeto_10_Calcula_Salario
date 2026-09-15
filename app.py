e1fbfbb9850c723762623001d96fa7dd04f4b345
bibiiiella <gabriellamarinho396@gmail.com>
2026-09-15 16:58:43 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+salario_hora = float(input("Digite o quanto você ganha por hora: "))
+horas_trabalho = float(input("Digite o quanto de horas você trabalha: "))
+
+salario = salario_hora*horas_trabalho
+
+print(f"O seu salario é de: {salario}")
